942f36785f8a9ccc179c82a2babc2b37a9d74f3c
sabitbinazad <sabitbinazad01@gmail.com>
2024-12-13 09:50:14 -0500

    add django files

---
+++ ormtemplate/settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'F2VtY9HxozuLGunAA6IUxxSW',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'standalone',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
